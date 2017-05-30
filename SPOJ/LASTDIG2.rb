78185eba7ee7673b627ace4c4ad234303a40cbc3
karthickpdy <karthickpdy@gmail.com>
2017-05-30 23:43:27 +0530

    COde

--- SPOJ/LASTDIG2.rb
+++ SPOJ/LASTDIG2.rb
@@ -1,19 +1,22 @@
 t = gets.to_i
 
-@same = [0,1,5,6]
-@cyclicity = {3 => 4,4 => 2,7 => 4, 8 => 4, 9 => 2}
+same = [0,1,5,6]
+cyclicity = {2=>4,3 => 4,4 => 2,7 => 4, 8 => 4, 9 => 2}
 
-def last(a,b)
-	return 1 if b == 0
-	x = a % 10
-	if(@same.include?(x))
-		return x
-	else
-		b %= @cyclicity[x]
-		return (x ** b) % 10
-	end
-end
 t.times do 
-	a,b = gets.split(" ").map(&:to_i)
-	puts last(a,b)
+	a,b = gets.chomp.split(" ")
+
+	b = b.to_i
+	x = a[-1].to_i
+	# ans = 1
+	if b == 0
+		ans = 1	
+	elsif(same.include?(x))
+		ans = x
+	else
+		b = b % cyclicity[x]
+		b = cyclicity[x] if b == 0
+		ans = (x ** (b)) % 10
+	end
+	puts ans
 end
